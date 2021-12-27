f77340000931f4e9258bdb1ab82272fbbfc6e901
Adabala-Praveena <96720074+Adabala-Praveena@users.noreply.github.com>
2021-12-27 15:00:37 +0530

    Add set card game.

---
+++ ShowSetsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShowSetsViewController.h
+//  cardMatch
+//
+//  Created by Praveena on 06/09/18.
+//  Copyright © 2018 Praveena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SetCardViewController.h"
+@interface ShowSetsViewController : SetCardViewController
+@property(strong,nonatomic) NSArray * unmatched_cards;
+@end
